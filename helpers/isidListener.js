4894bc99b9f12040f673b3ec3ac451a3ba11835e
Chad A. Carino <ccarino@sundialbrands.com>
2019-06-20 16:47:44 -0400

    moved function definition for all exports in isidListener helper into the same object to make for easier overrides during testing. Also touched up some logic. Wrote UT logic for all pending but final 2 functions (and one unlikely, untestable edge case).

--- helpers/isidListener.js
+++ helpers/isidListener.js
@@ -1,148 +1,145 @@
 var Listener = require('../listener');
 
-function ensureGlobalListener(isid) {
-
-	if ('object' !== typeof global.Uwot.Listeners) {
-	
-		global.Uwot.Listeners = {};
-	
-	}
-	if ('object' !== typeof global.Uwot.Listeners[isid]) {
-	
-		global.Uwot.Listeners[isid] = {};
-	
-	}
-	return global.Uwot.Listeners[isid];
-
-}
-
-function removeGlobalListener(isid) {
-
-	try {
-	
-		delete global.Uwot.Listeners[isid];
-		return true;
-	
-	}
-	catch(e) {
-	
-		return false;
-	
-	}
-
-}
-
-function newIsidDefaultListener(isid) {
-
-	var globalListeners = ensureGlobalListener(isid);
-	if ('object' !== typeof globalListeners.default || null === globalListeners.default) {
-	
-		try {
-		
-			globalListeners.default = new Listener('default', isid);
-		
-		}
-		catch(e) {
-		
-			return e;
-		
-		}
-	
-	}
-	return globalListeners.default;
-
-}
-
-function moveListeners(currentIsid, newIsid) {
-
-	var currentListenersObj = ensureGlobalListener(currentIsid);
-	var currentListenerNames = Object.keys(currentListenersObj);
-	var newListenersObj = ensureGlobalListener(newIsid);
-	if (currentListenerNames.length < 1) {
-	
-		newIsidDefaultListener(newIsid);
-		return newListenersObj;
-	
-	}
-	else {
-	
-		for (let i = 0; i < currentListenerNames.length; i++) {
-		
-			var oldListener = currentListenersObj[currentListenerNames[i]];
-			var newListener = Object.assign({}, oldListener);
-			newListener.instanceSessionId = newIsid;
-			newListenersObj[currentListenerNames[i]] = newListener;
-			oldListener.disable();
-			if ((i + 1) >= currentListenerNames.length) {
-			
-				removeGlobalListener(currentIsid);
-				return newListenersObj;
-			
-			}
-		
-		}
-	
-	}
-
-}
-
-function enableExclusiveState(isid) {
-
-	var globalListeners = ensureGlobalListener(isid);
-	var disabledForExclusive = [];
-	var allListenerNames = Object.keys(globalListeners);
-	for (let i = 0; i < allListenerNames.length; i++) {
-	
-		if (globalListeners[allListenerNames[i]].type !== 'exclusive' && globalListeners[allListenerNames[i]].status === 'enabled') {
-		
-			disabledForExclusive.push(allListenerNames[i]);
-			globalListeners[allListenerNames[i]].disable();
-		
-		}
-		if ((i + 1) >= allListenerNames.length) {
-		
-			globalListeners.disabledForExclusive = disabledForExclusive;
-			return disabledForExclusive;
-		
-		}
-	
-	}
-
-}
-
-function disableExclusiveState(isid) {
-
-	var globalListeners = ensureGlobalListener(isid);
-	var enabledListeners = [];
-	if ('object' === globalListeners.disabledForExclusive && Array.isArray(globalListeners.disabledForExclusive) && globalListeners.disabledForExclusive.length > 0) {
-	
-		for (let i = 0; i < globalListeners.disabledForExclusive.length; i++) {
-		
-			globalListeners[globalListeners.disabledForExclusive[i]].enable;
-			enabledListeners.push(globalListeners.disabledForExclusive[i]);
-			if ((i + 1) >= globalListeners.length) {
-			
-				delete globalListeners.disabledForExclusive;
-				return enabledListeners;
-			
-			}
-		
-		}
-	
-	}
-	else {
-	
-		return enabledListeners;
-	
-	}
-
-}
-
 module.exports = {
-	ensureGlobalListener,
-	removeGlobalListener,
-	newIsidDefaultListener,
-	moveListeners,
-	enableExclusiveState,
-	disableExclusiveState
+
+	ensureGlobalListener(isid) {
+
+		if ('object' !== typeof global.Uwot.Listeners) {
+	
+			global.Uwot.Listeners = {};
+	
+		}
+		if ('object' !== typeof global.Uwot.Listeners[isid]) {
+	
+			global.Uwot.Listeners[isid] = {};
+	
+		}
+		return global.Uwot.Listeners[isid];
+
+	},
+
+	removeGlobalListener(isid) {
+
+		try {
+	
+			delete global.Uwot.Listeners[isid];
+			return true;
+	
+		}
+		// this is only really possible if global is not available...
+		catch(e) {
+	
+			return false;
+	
+		}
+
+	},
+
+	newIsidDefaultListener(isid) {
+
+		var globalListeners = this.ensureGlobalListener(isid);
+		if ('object' !== typeof globalListeners.default || null === globalListeners.default) {
+	
+			try {
+		
+				delete globalListeners.default;
+				globalListeners.default = new Listener('default', isid);
+		
+			}
+			catch(e) {
+		
+				return e;
+		
+			}
+	
+		}
+		return globalListeners.default;
+
+	},
+
+	moveListeners(currentIsid, newIsid) {
+
+		var currentListenersObj = this.ensureGlobalListener(currentIsid);
+		var currentListenerNames = Object.keys(currentListenersObj);
+		var newListenersObj = this.ensureGlobalListener(newIsid);
+		if (currentListenerNames.length < 1) {
+	
+			this.newIsidDefaultListener(newIsid);
+			return newListenersObj;
+	
+		}
+		else {
+	
+			for (let i = 0; i < currentListenerNames.length; i++) {
+		
+				var oldListener = currentListenersObj[currentListenerNames[i]];
+				var newListener = Object.assign({}, oldListener);
+				newListener.isid = newIsid;
+				newListenersObj[currentListenerNames[i]] = newListener;
+				oldListener.disable();
+				if ((i + 1) >= currentListenerNames.length) {
+			
+					this.removeGlobalListener(currentIsid);
+					return newListenersObj;
+			
+				}
+		
+			}
+	
+		}
+
+	},
+
+	enableExclusiveState(isid) {
+
+		var globalListeners = this.ensureGlobalListener(isid);
+		var disabledForExclusive = [];
+		var allListenerNames = Object.keys(globalListeners);
+		for (let i = 0; i < allListenerNames.length; i++) {
+	
+			if (globalListeners[allListenerNames[i]].type !== 'exclusive' && globalListeners[allListenerNames[i]].status === 'enabled') {
+		
+				disabledForExclusive.push(allListenerNames[i]);
+				globalListeners[allListenerNames[i]].disable();
+		
+			}
+			if ((i + 1) >= allListenerNames.length) {
+		
+				globalListeners.disabledForExclusive = disabledForExclusive;
+				return disabledForExclusive;
+		
+			}
+	
+		}
+
+	},
+
+	disableExclusiveState(isid) {
+
+		var globalListeners = this.ensureGlobalListener(isid);
+		var enabledListeners = [];
+		if ('object' === globalListeners.disabledForExclusive && Array.isArray(globalListeners.disabledForExclusive) && globalListeners.disabledForExclusive.length > 0) {
+	
+			for (let i = 0; i < globalListeners.disabledForExclusive.length; i++) {
+		
+				globalListeners[globalListeners.disabledForExclusive[i]].enable;
+				enabledListeners.push(globalListeners.disabledForExclusive[i]);
+				if ((i + 1) >= globalListeners.length) {
+			
+					delete globalListeners.disabledForExclusive;
+					return enabledListeners;
+			
+				}
+		
+			}
+	
+		}
+		else {
+	
+			return enabledListeners;
+	
+		}
+
+	}
+
 };
--- test/helpers.isidListener.spec.js
+++ test/helpers.isidListener.spec.js
@@ -10,9 +10,21 @@ var isidListener = require('../helpers/isidListener');
 
 describe('isidListener.js', function() {
 
+	var testIsid, testDefaultListener;
 	before(function() {
 	
 		globalSetupHelper.initConstants();
+		testIsid = 'thefrogkermit';
+		testDefaultListener = {
+			name: 'default',
+			isid: testIsid,
+			type: 'default'
+		};
+	
+	});
+	afterEach(function() {
+	
+		delete global.Uwot.Listeners[testIsid];
 	
 	});
 	describe('ensureGlobalListener(isid)', function() {
@@ -22,10 +34,38 @@ describe('isidListener.js', function() {
 			expect(isidListener.ensureGlobalListener).to.be.a('function');
 		
 		});
-		it('should create an empty object as value of global.Uwot.Listeners if it is not already an object');
-		it('should create an empty object as value of global.Uwot.Listeners[isid] if it is not already an object');
-		it('should return a reference to global.Uwot.Listeners[isid], which should be an empty object, if it did not exist prior to being called');
-		it('should return a reference to global.Uwot.Listeners[isid], which should be the already existing object, if it existed prior to being called');
+		it('should create an empty object as value of global.Uwot.Listeners if it is not already an object', function() {
+		
+			delete global.Uwot.Listeners;
+			isidListener.ensureGlobalListener(testIsid);
+			expect(global.Uwot.Listeners).to.be.an('object').that.is.not.null;
+		
+		});
+		it('should create an empty object as value of global.Uwot.Listeners[isid] if it is not already an object', function() {
+		
+			delete global.Uwot.Listeners[testIsid];
+			isidListener.ensureGlobalListener(testIsid);
+			expect(global.Uwot.Listeners[testIsid]).to.be.an('object').that.is.not.null;
+		
+		});
+		it('should return a reference to global.Uwot.Listeners[isid], which should be an empty object, if it did not exist prior to being called', function() {
+		
+			delete global.Uwot.Listeners[testIsid];
+			isidListener.ensureGlobalListener(testIsid);
+			expect(global.Uwot.Listeners[testIsid]).to.deep.equal({});
+		
+		});
+		it('should return a reference to global.Uwot.Listeners[isid], which should be the already existing object, if it existed prior to being called', function() {
+		
+			var lilypadListeners = {
+				'default': testDefaultListener
+			};
+			global.Uwot.Listeners[testIsid] = lilypadListeners;
+			var globalListeners = isidListener.ensureGlobalListener(testIsid);
+			expect(global.Uwot.Listeners[testIsid]).to.deep.equal(lilypadListeners);
+			expect(globalListeners).to.deep.equal(lilypadListeners)
+		
+		});
 		
 	});
 	describe('removeGlobalListener(isid)', function() {
@@ -35,8 +75,21 @@ describe('isidListener.js', function() {
 			expect(isidListener.removeGlobalListener).to.be.a('function');
 		
 		});
-		it('should attempt to remove global.Uwot.Listeners[isid]');
-		it('should return true if attempt was successful and threw no errors');
+		it('should attempt to remove global.Uwot.Listeners[isid]', function() {
+		
+			isidListener.ensureGlobalListener(testIsid);
+			isidListener.removeGlobalListener(testIsid);
+			expect(global.Uwot.Listeners[testIsid]).to.be.undefined;
+		
+		});
+		it('should return true if attempt was successful and threw no errors', function() {
+		
+			isidListener.ensureGlobalListener(testIsid);
+			var wasRemoved = isidListener.removeGlobalListener(testIsid);
+			expect(wasRemoved).to.be.true;
+		
+		});
+		// this case is not really testable, or probable.
 		it('should return false if attempt threw an error');
 		
 	});
@@ -47,10 +100,90 @@ describe('isidListener.js', function() {
 			expect(isidListener.newIsidDefaultListener).to.be.a('function');
 		
 		});
-		it('should call ensureGlobalListener');
-		it('should set global.Uwot.Listeners[isid].default to a new instance of Listener class if current value is not a non-null object');
-		it('should return an Error if instantiating the default Listener throws an error');
-		it('should return the Listener global.Uwot.Listeners[isid].default if it was created without error or if it already existed');
+		it('should call ensureGlobalListener', function() {
+		
+			var ensureGlobalListenerStub = sinon.stub(isidListener, 'ensureGlobalListener').throws(new Error('test ensureGlobalListener error'));
+			try {
+			
+				isidListener.newIsidDefaultListener()
+				throw new Error('this is not the error you are looking for');
+			
+			}
+			catch(e) {
+			
+				ensureGlobalListenerStub.restore();
+				expect(e).to.be.an.instanceof(Error).with.property('message').that.equals('test ensureGlobalListener error');
+			
+			}
+		
+		});
+		it('should set global.Uwot.Listeners[isid].default to a new instance of Listener class if current value is not a non-null object', function() {
+		
+			var ensureGlobalListenerStub = sinon.stub(isidListener, 'ensureGlobalListener').callsFake(function setGlobal(isid) {
+			
+				if ('object' !== typeof global.Uwot.Listeners[isid]) {
+				
+					global.Uwot.Listeners[isid] = {};
+				
+				}
+				return global.Uwot.Listeners[isid];
+			
+			});
+			delete global.Uwot.Listeners[testIsid];
+			isidListener.newIsidDefaultListener(testIsid);
+			expect(global.Uwot.Listeners[testIsid].default).to.be.an('object').with.property('isid').that.equals(testIsid);
+			expect(global.Uwot.Listeners[testIsid].default.constructor.name).to.equal('UwotListener');
+			global.Uwot.Listeners[testIsid].default = null;
+			isidListener.newIsidDefaultListener(testIsid);
+			expect(global.Uwot.Listeners[testIsid].default).to.be.an('object').with.property('isid').that.equals(testIsid);
+			expect(global.Uwot.Listeners[testIsid].default.constructor.name).to.equal('UwotListener');
+			ensureGlobalListenerStub.restore();
+			delete global.Uwot.Listeners[testIsid];
+		
+		});
+		it('should return an Error if instantiating the default Listener throws an error', function() {
+		
+			var ensureGlobalListenerStub = sinon.stub(isidListener, 'ensureGlobalListener').callsFake(function setGlobal(isid) {
+			
+				if ('object' !== typeof global.Uwot.Listeners[isid]) {
+				
+					global.Uwot.Listeners[isid] = {};
+				
+				}
+				return global.Uwot.Listeners[isid];
+			
+			});
+			expect(isidListener.newIsidDefaultListener(1234)).to.be.an.instanceof(TypeError).with.property('message').that.equals('invalid instanceSessionId passed to UwotCliListener contstructor');
+			delete global.Uwot.Listeners[testIsid];
+			delete global.Uwot.Listeners[1324];
+			ensureGlobalListenerStub.restore();
+		
+		});
+		it('should return the Listener global.Uwot.Listeners[isid].default if it was created without error or if it already existed', function() {
+		
+			var ensureGlobalListenerStub = sinon.stub(isidListener, 'ensureGlobalListener').callsFake(function setGlobal(isid) {
+			
+				if ('object' !== typeof global.Uwot.Listeners[isid]) {
+				
+					global.Uwot.Listeners[isid] = {};
+				
+				}
+				return global.Uwot.Listeners[isid];
+			
+			});
+			global.Uwot.Listeners[testIsid] = {};
+			global.Uwot.Listeners[testIsid].default = testDefaultListener;
+			var returnedListener;
+			returnedListener = isidListener.newIsidDefaultListener(testIsid);
+			expect(returnedListener).to.deep.equal(testDefaultListener);
+			delete global.Uwot.Listeners[testIsid].default;
+			returnedListener = isidListener.newIsidDefaultListener(testIsid);
+			expect(returnedListener).to.be.an('object').with.property('isid').that.equals(testIsid);
+			expect(returnedListener.constructor.name).to.equal('UwotListener');
+			ensureGlobalListenerStub.restore();
+			delete global.Uwot.Listeners[testIsid];
+		
+		});
 		
 	});
 	describe('moveListeners(currentIsid, newIsid)', function() {
@@ -60,14 +193,257 @@ describe('isidListener.js', function() {
 			expect(isidListener.moveListeners).to.be.a('function');
 		
 		});
-		it('should call ensureGlobalListener for isids in both args');
-		it('should call newIsidDefaultListener for the new isid and return global.Uwot.Listeners[newIsid] if the old isid had no existing global listeners');
-		it('should create new listeners matching the old isid globals');
-		it('should set the isid to newIsid value for all new listeners');
-		it('should add all new listeners to the global listener object for newIsid');
-		it('should disable all old isid listeners prior to removal');
-		it('should remove global.Uwot.Listeners[currentIsid] by calling removeGlobalListener');
-		it('should return a reference to global.Uwot.Listeners[newIsid]');
+		it('should call ensureGlobalListener for isids in both args', function() {
+		
+			var testNewIsid = 'ladypig';
+			var ensureGlobalListenerStub = sinon.stub(isidListener, 'ensureGlobalListener').callsFake(function setGlobal(isid) {
+			
+				if ('object' !== typeof global.Uwot.Listeners[isid]) {
+				
+					global.Uwot.Listeners[isid] = {};
+				
+				}
+				return global.Uwot.Listeners[isid];
+			
+			});
+			isidListener.moveListeners(testIsid, testNewIsid);
+			delete global.Uwot.Listeners[testIsid];
+			delete global.Uwot.Listeners[testNewIsid];
+			expect(ensureGlobalListenerStub.calledWith(testIsid)).to.be.true;
+			expect(ensureGlobalListenerStub.calledWith(testNewIsid)).to.be.true;
+			ensureGlobalListenerStub.restore();
+		
+		});
+		it('should call newIsidDefaultListener for the new isid and return global.Uwot.Listeners[newIsid] if the old isid had no existing global listeners', function() {
+		
+			var testNewIsid = 'ladypig';
+			var ensureGlobalListenerStub = sinon.stub(isidListener, 'ensureGlobalListener').callsFake(function setGlobal(isid) {
+			
+				if ('object' !== typeof global.Uwot.Listeners[isid]) {
+				
+					global.Uwot.Listeners[isid] = {};
+				
+				}
+				return global.Uwot.Listeners[isid];
+			
+			});
+			var newIsidDefaultListenerStub = sinon.stub(isidListener, 'newIsidDefaultListener').callsFake(function setFakeGlobal(isid) {
+			
+				global.Uwot.Listeners[isid].default = testDefaultListener;
+				global.Uwot.Listeners[isid].default.isid = isid;
+				return global.Uwot.Listeners[isid].default;
+			
+			});
+			delete global.Uwot.Listeners[testIsid];
+			delete global.Uwot.Listeners[testNewIsid];
+			var moveResult = isidListener.moveListeners(testIsid, testNewIsid);
+			expect(newIsidDefaultListenerStub.calledWith(testNewIsid)).to.be.true;
+			expect(moveResult).to.deep.equal({default: testDefaultListener});
+			delete global.Uwot.Listeners[testIsid];
+			delete global.Uwot.Listeners[testNewIsid];
+			ensureGlobalListenerStub.restore();
+			newIsidDefaultListenerStub.restore();
+		
+		});
+		it('should create new listeners matching the old isid globals', function() {
+		
+			var ensureGlobalListenerStub = sinon.stub(isidListener, 'ensureGlobalListener').callsFake(function setGlobal(isid) {
+			
+				if ('object' !== typeof global.Uwot.Listeners[isid]) {
+				
+					global.Uwot.Listeners[isid] = {};
+				
+				}
+				return global.Uwot.Listeners[isid];
+			
+			});
+			var testNewIsid = 'ladypig';
+			var testListener1 = Object.assign({}, testDefaultListener);
+			testListener1.disable = function() { return; };
+			var testListener2 = Object.assign({}, testListener1);
+			testListener2.name = 'testadd';
+			testListener2.type = 'additional';
+			global.Uwot.Listeners[testIsid] = {
+				default: testListener1,
+				testadd: testListener2
+			};
+			var finalListeners = [testListener1.name, testListener2.name]
+			delete global.Uwot.Listeners[testNewIsid];
+			var moveResult = isidListener.moveListeners(testIsid, testNewIsid);
+			expect(Object.keys(moveResult)).to.deep.equal(finalListeners);
+			delete global.Uwot.Listeners[testIsid];
+			delete global.Uwot.Listeners[testNewIsid];
+			ensureGlobalListenerStub.restore();
+		
+		});
+		it('should set the isid to newIsid value for all new listeners', function() {
+		
+			var ensureGlobalListenerStub = sinon.stub(isidListener, 'ensureGlobalListener').callsFake(function setGlobal(isid) {
+			
+				if ('object' !== typeof global.Uwot.Listeners[isid]) {
+				
+					global.Uwot.Listeners[isid] = {};
+				
+				}
+				return global.Uwot.Listeners[isid];
+			
+			});
+			var testNewIsid = 'ladypig';
+			var testListener1 = Object.assign({}, testDefaultListener);
+			testListener1.disable = function() { return; };
+			var testListener2 = Object.assign({}, testListener1);
+			testListener2.name = 'testadd';
+			testListener2.type = 'additional';
+			global.Uwot.Listeners[testIsid] = {
+				default: testListener1,
+				testadd: testListener2
+			};
+			delete global.Uwot.Listeners[testNewIsid];
+			var moveResult = isidListener.moveListeners(testIsid, testNewIsid);
+			expect(moveResult.default.isid).to.equal(testNewIsid);
+			expect(moveResult[testListener2.name].isid).to.equal(testNewIsid);
+			delete global.Uwot.Listeners[testIsid];
+			delete global.Uwot.Listeners[testNewIsid];
+			ensureGlobalListenerStub.restore();
+		
+		});
+		it('should add all new listeners to the global listener object for newIsid', function() {
+		
+			var ensureGlobalListenerStub = sinon.stub(isidListener, 'ensureGlobalListener').callsFake(function setGlobal(isid) {
+			
+				if ('object' !== typeof global.Uwot.Listeners[isid]) {
+				
+					global.Uwot.Listeners[isid] = {};
+				
+				}
+				return global.Uwot.Listeners[isid];
+			
+			});
+			var testNewIsid = 'ladypig';
+			var testListener1 = Object.assign({}, testDefaultListener);
+			testListener1.disable = function() { return; };
+			var testListener2 = Object.assign({}, testListener1);
+			testListener2.name = 'testadd';
+			testListener2.type = 'additional';
+			global.Uwot.Listeners[testIsid] = {
+				default: testListener1,
+				testadd: testListener2
+			};
+			var finalListeners = [testListener1.name, testListener2.name]
+			delete global.Uwot.Listeners[testNewIsid];
+			var moveResult = isidListener.moveListeners(testIsid, testNewIsid);
+			expect(Object.keys(global.Uwot.Listeners[testNewIsid])).to.deep.equal(finalListeners);
+			delete global.Uwot.Listeners[testIsid];
+			delete global.Uwot.Listeners[testNewIsid];
+			ensureGlobalListenerStub.restore();
+		
+		});
+		it('should disable all old isid listeners prior to removal', function() {
+		
+			var ensureGlobalListenerStub = sinon.stub(isidListener, 'ensureGlobalListener').callsFake(function setGlobal(isid) {
+			
+				if ('object' !== typeof global.Uwot.Listeners[isid]) {
+				
+					global.Uwot.Listeners[isid] = {};
+				
+				}
+				return global.Uwot.Listeners[isid];
+			
+			});
+			var testNewIsid = 'ladypig';
+			var disabledListeners = [];
+			var testListener1 = Object.assign({}, testDefaultListener);
+			testListener1.disable = function() {
+			
+				disabledListeners.push(this.isid + '-' + this.name);
+				return;
+			
+			};
+			var testListener2 = Object.assign({}, testListener1);
+			testListener2.name = 'testadd';
+			testListener2.type = 'additional';
+			global.Uwot.Listeners[testIsid] = {
+				default: testListener1,
+				testadd: testListener2
+			};
+			var finalDisabled = [testListener1.isid + '-' + testListener1.name, testListener2.isid + '-' + testListener2.name]
+			delete global.Uwot.Listeners[testNewIsid];
+			var moveResult = isidListener.moveListeners(testIsid, testNewIsid);
+			expect(disabledListeners).to.deep.equal(finalDisabled);
+			delete global.Uwot.Listeners[testIsid];
+			delete global.Uwot.Listeners[testNewIsid];
+			ensureGlobalListenerStub.restore();
+		
+		});
+		it('should remove global.Uwot.Listeners[currentIsid] by calling removeGlobalListener', function() {
+		
+			var ensureGlobalListenerStub = sinon.stub(isidListener, 'ensureGlobalListener').callsFake(function setGlobal(isid) {
+			
+				if ('object' !== typeof global.Uwot.Listeners[isid]) {
+				
+					global.Uwot.Listeners[isid] = {};
+				
+				}
+				return global.Uwot.Listeners[isid];
+			
+			});
+			var removeGlobalListenerStub = sinon.stub(isidListener, 'removeGlobalListener').callsFake(function removeListener(isid) {
+			
+				delete global.Uwot.Listeners[isid];
+				return true;
+			
+			});
+			var testNewIsid = 'ladypig';
+			var testListener1 = Object.assign({}, testDefaultListener);
+			testListener1.disable = function() { return; };
+			var testListener2 = Object.assign({}, testListener1);
+			testListener2.name = 'testadd';
+			testListener2.type = 'additional';
+			global.Uwot.Listeners[testIsid] = {
+				default: testListener1,
+				testadd: testListener2
+			};
+			var finalListeners = [testListener1.name, testListener2.name]
+			delete global.Uwot.Listeners[testNewIsid];
+			isidListener.moveListeners(testIsid, testNewIsid);
+			expect(global.Uwot.Listeners[testIsid]).to.be.undefined;
+			expect(removeGlobalListenerStub.calledWith(testIsid)).to.be.true;
+			delete global.Uwot.Listeners[testIsid];
+			delete global.Uwot.Listeners[testNewIsid];
+			ensureGlobalListenerStub.restore();
+			removeGlobalListenerStub.restore();
+		
+		});
+		it('should return a reference to global.Uwot.Listeners[newIsid]', function() {
+		
+			var ensureGlobalListenerStub = sinon.stub(isidListener, 'ensureGlobalListener').callsFake(function setGlobal(isid) {
+			
+				if ('object' !== typeof global.Uwot.Listeners[isid]) {
+				
+					global.Uwot.Listeners[isid] = {};
+				
+				}
+				return global.Uwot.Listeners[isid];
+			
+			});
+			var testNewIsid = 'ladypig';
+			var testListener1 = Object.assign({}, testDefaultListener);
+			testListener1.disable = function() { return; };
+			var testListener2 = Object.assign({}, testListener1);
+			testListener2.name = 'testadd';
+			testListener2.type = 'additional';
+			global.Uwot.Listeners[testIsid] = {
+				default: testListener1,
+				testadd: testListener2
+			};
+			delete global.Uwot.Listeners[testNewIsid];
+			var moveResult = isidListener.moveListeners(testIsid, testNewIsid);
+			expect(moveResult).to.deep.equal(global.Uwot.Listeners[testNewIsid]);
+			delete global.Uwot.Listeners[testIsid];
+			delete global.Uwot.Listeners[testNewIsid];
+			ensureGlobalListenerStub.restore();
+		
+		});
 	
 	});
 	describe('enableExclusiveState(isid)', function() {
